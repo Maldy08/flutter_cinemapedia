58c08d4541fdab01014e3c69417447577b9ad572
CARLOS MALDONADO <karloz_0986@hotmail.com>
2023-03-06 15:42:20 -0800

    final seccion 12

---
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_cinemapedia/config/constants/environment.dart';
+import 'package:flutter_cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:flutter_cinemapedia/domain/entities/movie.dart';
+import 'package:flutter_cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:flutter_cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
+
+class MoviedbDataSource extends MovieDataSource {
+  final dio = Dio(BaseOptions(
+    baseUrl: 'https://api.themoviedb.org/3',
+    queryParameters: {
+      'api_key': Environment.movieDBKey,
+      'language': 'es-MX',
+    },
+  ));
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response = await dio.get('/movie/now_playing');
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies = movieDBResponse.results
+        .where((moviedb) => moviedb.posterPath != 'no-poster')
+        .map((moviedb) => MovieMapper.movieDBToEntity(moviedb))
+        .toList();
+    return movies;
+  }
+}
---
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_cinemapedia/infrastructure/datasources/moviedb_datasource.dart';
+import 'package:flutter_cinemapedia/infrastructure/repositories/movie_repository_impl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Este repositorio es inmutable.
+final movieRepositoryProvider = Provider((ref) {
+  return MovieRepositoryImpl(MoviedbDataSource());
+});
