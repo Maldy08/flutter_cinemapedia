5ad2c551178e4c7900590951272520ac08ea0de3
CARLOS MALDONADO <karloz_0986@hotmail.com>
2023-03-06 17:03:22 -0800

    swiper-pagination

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:flutter_cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -35,15 +36,13 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
 
-    if (nowPlayingMovies.length == 0) return CircularProgressIndicator();
+    if (nowPlayingMovies.isEmpty) return const CircularProgressIndicator();
 
-    return ListView.builder(
-        itemCount: nowPlayingMovies.length,
-        itemBuilder: (context, index) {
-          final movie = nowPlayingMovies[index];
-          return ListTile(
-            title: Text(movie.title),
-          );
-        });
+    return Column(
+      children: [
+        const CustomAppbar(),
+        MoviesSlideshow(movies: nowPlayingMovies),
+      ],
+    );
   }
 }
